e761976d56cc0bdb2b40f58a70309dd17b75a6e2
zqbq099 <zqbq099@gmail.com>
2026-04-16 23:19:07 +0300

    Create Color.kt

---
+++ app/src/main/java/com/ejar/app/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.ejar.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NavyBackground = Color(0xFF050B18)
+val LuxuryGold = Color(0xFFD4AF37)
+val White50 = Color(0x80FFFFFF)
+val White20 = Color(0x33FFFFFF)
+val White10 = Color(0x1AFFFFFF)
+val BlackOverlay = Color(0x80000000)
+val GoldDark = Color(0xFFB8960C)
+val ErrorRed = Color(0xFFCF6679)
+val SuccessGreen = Color(0xFF4CAF50)
