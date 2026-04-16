23fa42d1799e7c1ec00ea604aeaf85fcc00e65dd
zqbq099 <zqbq099@gmail.com>
2026-04-16 23:25:06 +0300

    Add Firebase module for dependency injection

---
+++ app/src/main/java/com/ejar/app/di/FirebaseModule.kt
@@ -0,0 +1,21 @@
+package com.ejar.app.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+    @Provides @Singleton fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+    @Provides @Singleton fun provideFirebaseFirestore(): FirebaseFirestore = Firebase.firestore
+    @Provides @Singleton fun provideFirebaseStorage(): FirebaseStorage = Firebase.storage
+}
